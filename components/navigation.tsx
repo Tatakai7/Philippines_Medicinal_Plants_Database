71ded00778104d4b3c0642971fbbdda44dbde248
unknown <raymondcaberte7@gmail.com>
2026-02-01 12:30:28 +0800

    4th file

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -18,7 +18,6 @@ export default function Navigation() {
               <div className="w-8 h-8 rounded-lg bg-primary flex items-center justify-center text-white font-bold">
                 🌿
               </div>
-              <span className="font-bold text-lg hidden sm:inline text-foreground">Hilot Database</span>
             </Link>
           </div>
 
